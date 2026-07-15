0a2582f17a9c0236dfe7050a34ed0bb19470502d
Jacek Jakubik <jacek.jakubik@tellent.com>
2026-07-15 08:59:23 +0200

    Make viex httpoison 3.0 compatibile

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Viex.Mixfile do
   def project do
     [
       app: :viex,
-      version: "0.3.4",
-      elixir: "~> 1.4",
+      version: "0.4.0",
+      elixir: "~> 1.15",
       description: "Elixir package to validate European VAT numbers with the VIES service.",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
@@ -35,12 +35,12 @@ defmodule Viex.Mixfile do
 
   defp deps do
     [
-      {:httpoison, "~> 2.0"},
+      {:httpoison, "~> 3.0"},
       {:floki, "~> 0.30"},
-      {:exvcr, "~> 0.8", only: :test},
-      {:excoveralls, "~> 0.5", only: :test},
-      {:ex_doc, "~> 0.14", only: :dev},
-      {:earmark, "~> 1.1", only: :dev},
+      {:exvcr, github: "rindek/exvcr", ref: "master", only: [:test]},
+      {:excoveralls, "~> 0.5", only: [:test]},
+      {:ex_doc, "~> 0.14", only: [:dev]},
+      {:earmark, "~> 1.1", only: [:dev]},
       {:credo, "~> 0.5", only: [:dev, :test]}
     ]
   end
